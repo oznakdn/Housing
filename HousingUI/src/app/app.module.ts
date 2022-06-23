9752d19fcf276f123d6573185feb0f20c2683404
Ozan Akaydin <ozanakaydin@gmail.com>
2022-06-23 20:22:38 +0300

    Add nav-bar and properties components

--- HousingUI/src/app/app.module.ts
+++ HousingUI/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './properties/property-card/property-card.component';
+import { PropertyListComponent } from './properties/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyCardComponent,
+    PropertyListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
